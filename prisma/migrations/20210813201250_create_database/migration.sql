7c424ee0ec990275e17f5a6f553a88f00151bdc0
matheus correa <mathec@live.com>
2021-08-13 17:13:36 -0300

    fix: reset database

--- prisma/migrations/20210813201250_create_database/migration.sql
+++ prisma/migrations/20210813201250_create_database/migration.sql
@@ -13,7 +13,7 @@ CREATE TABLE "Company" (
     "id" SERIAL NOT NULL,
     "uuid" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "fileId" INTEGER NOT NULL,
+    "fileId" INTEGER,
 
     PRIMARY KEY ("id")
 );
@@ -25,12 +25,20 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "mail" TEXT NOT NULL,
     "salt" TEXT NOT NULL,
-    "tokens" JSONB NOT NULL,
     "rounds" INTEGER NOT NULL DEFAULT 10,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "fileId" INTEGER,
     "companyId" INTEGER NOT NULL,
-    "fileId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "token" TEXT NOT NULL,
+    "userId" INTEGER,
 
     PRIMARY KEY ("id")
 );
@@ -44,7 +52,7 @@ CREATE TABLE "Product" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "productsValidationsId" INTEGER NOT NULL,
     "companyId" INTEGER NOT NULL,
-    "fileId" INTEGER NOT NULL,
+    "fileId" INTEGER,
 
     PRIMARY KEY ("id")
 );
@@ -71,11 +79,41 @@ CREATE TABLE "Log" (
     PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Laborer" (
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "mail" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "laborerProfileId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "LaborerProfile" (
+    "id" SERIAL NOT NULL,
+    "uuid" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "File.uuid_unique" ON "File"("uuid");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Company.uuid_unique" ON "Company"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company.name_unique" ON "Company"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User.uuid_unique" ON "User"("uuid");
@@ -92,14 +130,26 @@ CREATE UNIQUE INDEX "ProductValidation.uuid_unique" ON "ProductValidation"("uuid
 -- CreateIndex
 CREATE UNIQUE INDEX "Log.uuid_unique" ON "Log"("uuid");
 
--- AddForeignKey
-ALTER TABLE "Company" ADD FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Laborer.uuid_unique" ON "Laborer"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LaborerProfile.uuid_unique" ON "LaborerProfile"("uuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "LaborerProfile.description_unique" ON "LaborerProfile"("description");
 
 -- AddForeignKey
-ALTER TABLE "User" ADD FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Company" ADD FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "User" ADD FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Product" ADD FOREIGN KEY ("productsValidationsId") REFERENCES "ProductValidation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -108,7 +158,10 @@ ALTER TABLE "Product" ADD FOREIGN KEY ("productsValidationsId") REFERENCES "Prod
 ALTER TABLE "Product" ADD FOREIGN KEY ("companyId") REFERENCES "Company"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Product" ADD FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Product" ADD FOREIGN KEY ("fileId") REFERENCES "File"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Log" ADD FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Laborer" ADD FOREIGN KEY ("laborerProfileId") REFERENCES "LaborerProfile"("id") ON DELETE CASCADE ON UPDATE CASCADE;
